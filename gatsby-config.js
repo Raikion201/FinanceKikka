5988f8582401075ddee7b502e140d2caf01c3f14
Raikion201 <haitrieukt123@gmail.com>
2023-01-18 21:10:35 +0700

    final

---
+++ gatsby-config.js
@@ -0,0 +1,30 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+
+module.exports = {
+  flags: {
+    DEV_SSR: true
+  },
+  siteMetadata: {
+    title: "Kinka Finance",
+    description: "Official Contentful Gatsby Starter",
+  },
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-image",
+    "gatsby-plugin-fontawesome-css",
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        host: process.env.CONTENTFUL_HOST
+      },
+    },
+  ],
+};
